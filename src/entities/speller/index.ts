9181a1ebcd901e8e36dbfaebcb17a7cb0b24015b
bicochan <chanii8@naver.com>
2025-01-23 02:35:16 +0900

    feat: 맞춤법 검사 API 데이터 바인딩 (#56)

--- src/entities/speller/index.ts
+++ src/entities/speller/index.ts
@@ -1,4 +1,8 @@
-export type { CheckResponse } from './model/api-interface'
+export type {
+  CheckResponse,
+  ErrorInfo,
+  CorrectMethod,
+} from './model/api-interface'
 export { checkApiServiceInstance } from './model/api-service'
 export { useSpeller } from './model/use-speller'
 export { spellerReducer, type SpellerState } from './model/speller-slice'
