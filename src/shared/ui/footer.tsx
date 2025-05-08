e8357a4e423491caf4f6539ad252bcaf95b60a2b
suhyeoonn <suhyeon1740@gmail.com>
2025-05-09 05:59:47 +0900

    fix: 광고 영역의 배경색 제거 (#204)

--- src/shared/ui/footer.tsx
+++ src/shared/ui/footer.tsx
@@ -72,7 +72,7 @@ const Footer = () => {
           <div className='hidden h-[90px] w-[728px] pc:block'>
             {/* 광고 영역 */}
             <GoogleAdSense
-              className='mb-1 hidden h-[50px] w-80 rounded-sm bg-slate-300 p-4 text-center tab:mb-0 tab:h-[90px] tab:w-[728px] pc:block'
+              className='mb-1 hidden h-[50px] w-80 rounded-sm p-4 text-center tab:mb-0 tab:h-[90px] tab:w-[728px] pc:block'
               data-ad-slot='4790060150'
               data-full-width-responsive='true'
             />
