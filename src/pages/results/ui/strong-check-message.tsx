508960b83de1f09cdc83998a0829db08c6c1b82c
bicochan <chanii8@naver.com>
2025-06-18 10:03:29 +0900

    fix: 강한 검사 적용 문구 ui 깨지는 현상 해결 (#233)

--- src/pages/results/ui/strong-check-message.tsx
+++ src/pages/results/ui/strong-check-message.tsx
@@ -1,14 +1,20 @@
 'use client'
 
 import { useSpeller } from '@/entities/speller'
+import { cn } from '@/shared/lib/tailwind-merge'
 
 const StrongCheckMessage = () => {
   const {
-    response: { requestedWithStrictMode },
+    response: { requestedWithStrictMode, totalPageCnt },
   } = useSpeller()
 
   return (
-    <div className='flex w-full items-center justify-end text-base font-medium leading-[150%] tracking-[-0.02rem] text-slate-300 tab:absolute tab:right-0 tab:w-auto pc:text-xl pc:tracking-[-0.025rem]'>
+    <div
+      className={cn(
+        'flex w-full items-center justify-end text-base font-medium leading-[150%] tracking-[-0.02rem] text-slate-300 pc:text-xl pc:tracking-[-0.025rem]',
+        totalPageCnt > 1 && 'tab:absolute tab:right-0',
+      )}
+    >
       {requestedWithStrictMode
         ? '강한 검사가 적용되었습니다.'
         : '강한 검사가 적용되지 않았습니다.'}
