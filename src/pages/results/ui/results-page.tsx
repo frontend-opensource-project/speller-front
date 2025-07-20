d3b9801156777661011f1b75b2026f1b2e950b79
bicochan <chanii8@naver.com>
2025-07-20 19:27:23 +0900

    design: 전역 컴포넌트 여백(padding/margin) 조정 (#246)

--- src/pages/results/ui/results-page.tsx
+++ src/pages/results/ui/results-page.tsx
@@ -50,8 +50,8 @@ const ResultsPage = ({ pageParam = '1' }: ResultsPageProps) => {
 
   return (
     <SpellerRefsProvider>
-      <ContentLayout className='pb-8 tab:pb-[2.625rem] pc:pb-12'>
-        <div className='sticky -top-2 z-10 flex min-h-[1.625rem] items-center justify-between bg-background pb-2 pt-4 tab:-top-4 tab:justify-center tab:pt-[1.75rem] pc:relative pc:top-0 pc:min-h-8 pc:pb-3 pc:pt-8'>
+      <ContentLayout className='pb-4 tab:pb-10 pc:pb-5'>
+        <div className='sticky -top-2 z-10 flex min-h-[1.625rem] items-center justify-between bg-background pb-2 pt-4 tab:-top-3 tab:justify-center tab:pt-[1.25rem] pc:relative pc:top-0 pc:min-h-8 pc:pb-3 pc:pt-6'>
           <Navigator currentPage={Number(pageParam)} />
           <StrongCheckMessage />
         </div>
@@ -61,7 +61,7 @@ const ResultsPage = ({ pageParam = '1' }: ResultsPageProps) => {
           ref={contentRef}
         >
           {/* 교정 문서*/}
-          <div className='content-visibility-auto flex min-h-[30.5rem] flex-col rounded-lg bg-white p-5 contain-strict tab:rounded-[1rem] tab:p-10 pc:w-1/2 pc:rounded-br-none pc:rounded-tr-none pc:pr-[1.25rem] pc-lg:pr-10'>
+          <div className='content-visibility-auto flex min-h-[30.5rem] flex-col rounded-lg bg-white p-4 contain-strict tab:rounded-[1rem] tab:p-5 pc:w-1/2 pc:rounded-br-none pc:rounded-tr-none pc:p-6'>
             <CorrectionContent />
             {/* 글자수 & 돌아가기, 복사하기 버튼 */}
             <ResultsControl />
@@ -70,7 +70,7 @@ const ResultsPage = ({ pageParam = '1' }: ResultsPageProps) => {
           {/* 맞춤법/문법 오류 */}
           <div
             className={cn(
-              'content-visibility-auto flex min-h-[30.5rem] flex-col rounded-lg border border-blue-500 bg-white px-5 pb-3.5 pt-[1.125rem] contain-strict tab:rounded-[1rem] tab:p-10 tab:pb-7 pc:w-1/2 pc:rounded-bl-none pc:rounded-tl-none pc:border-none pc:pb-10 pc:pl-[1.25rem] pc-lg:pl-10',
+              'content-visibility-auto flex min-h-[30.5rem] flex-col rounded-lg border border-blue-500 bg-white p-4 contain-strict tab:rounded-[1rem] tab:p-5 pc:w-1/2 pc:rounded-bl-none pc:rounded-tl-none pc:border-none pc:p-6',
               '@container',
             )}
           >
