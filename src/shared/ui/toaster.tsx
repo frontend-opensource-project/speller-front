b04eb7b50de9e1e04152e3948babc14aec435d0d
suhyeoonn <suhyeon1740@gmail.com>
2025-08-05 06:19:55 +0900

    fix: 토스트 위치 페이지 버튼 하단으로 변경 (#258)

--- src/shared/ui/toaster.tsx
+++ src/shared/ui/toaster.tsx
@@ -17,7 +17,7 @@ export function Toaster() {
   const { toasts } = useToast()
 
   return (
-    <ToastProvider swipeDirection='up' duration={3000}>
+    <ToastProvider swipeDirection='up' duration={2000}>
       {toasts.map(function ({ id, title, description, action, ...props }) {
         return (
           <Toast key={id} {...props}>
