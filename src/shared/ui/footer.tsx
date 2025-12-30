9d04588dd7351cce97f2cf27c664c55475385ea2
bicochan <chanii8@naver.com>
2025-12-30 07:56:47 +0000

    design: footer 영역에 문구 추가 및 불필요한 여백 제거 (#300)
    - footer 영역에 유료 사용자 전용 안내 문구 추가
    - footer 영역의 불필요한 여백 제거

--- src/shared/ui/footer.tsx
+++ src/shared/ui/footer.tsx
@@ -1,19 +1,15 @@
-'use client'
-
 import { cn } from '../lib/tailwind-merge'
 
 const Footer = () => {
   return (
-    <footer
-      className={cn(
-        'max-h-[14.5rem] bg-slate-200 tab:max-h-[17.4375rem] tab:min-h-[11.875rem] pc:mb-0 pc:max-h-[9.5rem] pc:min-h-[9.5rem]',
-      )}
-    >
-      <div
+    <footer className={cn('bg-slate-200 px-4 py-3')}>
+      <p
         className={cn(
-          'flex h-full flex-col items-center pc-lg:container pc:mx-auto pc:w-full pc:flex-row pc:justify-center pc:space-x-0 pc:px-[2.25rem] pc:py-2 pc-lg:space-x-0 pc-lg:px-[4.5rem]',
+          'text-center text-base font-bold text-slate-500 pc-lg:container pc:mx-auto',
         )}
-      ></div>
+      >
+        본 페이지는 유료 사용자 전용입니다. URL 공유 및 외부 유출을 금지합니다.
+      </p>
     </footer>
   )
 }
